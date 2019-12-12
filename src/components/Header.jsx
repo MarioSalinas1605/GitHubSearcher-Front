e9711566d4ca76f6c2818203e54232829e4d4151
MarioSalinas <mario_1605@live.com.mx>
2019-12-11 19:03:55 -0600

    feature/basic toggler navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,40 @@
 import React from "react";
+import "bootstrap/js/src/collapse.js";
 
 const Header = () => {
-  return <h1>This mi header</h1>;
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <a className="navbar-brand" href="#">
+        Navbar
+      </a>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav mr-auto">
+          <li className="nav-item active">
+            <a className="nav-link ml-5" href="#">
+              Home
+            </a>
+          </li>
+          <li className="nav-item ml-5">
+            <a className="nav-link" href="#">
+              Link
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 };
 
 export default Header;
