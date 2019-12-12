79ceafe7d5ef3ad94dd1aaa2120bbc4829b6b5ba
MarioSalinas <mario_1605@live.com.mx>
2019-12-11 19:08:36 -0600

    refactor/custom github searcher navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,9 +3,9 @@ import "bootstrap/js/src/collapse.js";
 
 const Header = () => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <a className="navbar-brand" href="#">
-        Navbar
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <a className="navbar-brand text-white" href="#">
+        GitHub Searcher
       </a>
       <button
         className="navbar-toggler"
@@ -22,13 +22,13 @@ const Header = () => {
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
         <ul className="navbar-nav mr-auto">
           <li className="nav-item active">
-            <a className="nav-link ml-5" href="#">
-              Home
+            <a className="nav-link ml-5 text-white" href="#">
+              Users
             </a>
           </li>
           <li className="nav-item ml-5">
-            <a className="nav-link" href="#">
-              Link
+            <a className="nav-link text-white" href="#">
+              Repositories
             </a>
           </li>
         </ul>
