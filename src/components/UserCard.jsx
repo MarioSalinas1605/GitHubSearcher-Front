abdf050c59f2a939f8f91469645d3eb5471ef31f
MarioSalinas <mario_1605@live.com.mx>
2019-12-13 09:15:12 -0600

    refactor/user card with info from api by props

--- src/components/UserCard.jsx
+++ src/components/UserCard.jsx
@@ -1,34 +1,20 @@
 import React from "react";
 
-const UserCard = () => {
+const UserCard = ({ user }) => {
   return (
     <>
-      <div className="card my-4 my-sm-5 p-2 bg-dark text-secondary">
-        <div className="row no-gutters">
-          <div className="col-md-4">
-            <p>This is my image</p>
-          </div>
-          <div className="col-md-8">
-            <div className="card-body">
-              <h5 className="card-title title-secondary-responsive">
-                This is the user Nickname
-              </h5>
-              <p className="card-text text-responsive">This the user name</p>
-              <p className="card-text text-responsive">
-                This the user description
-              </p>
-              <p className="card-text text-responsive">
-                <small className="text-muted">Last updated 3 mins ago</small>
-              </p>
-            </div>
-          </div>
-        </div>
-        <div className="row my-3">
-          <div className="col-12 mx-4">
-            <p>This are the public user repositories</p>
-            <p>This is the company of the user</p>
-            <p>This is the location of the user</p>
-          </div>
+      <div className="card my-4 my-sm-5 p-2 bg-secondary text-secondary">
+        <img
+          src={user.avatar_url}
+          className="card-img-top img-fluid"
+          alt="Avatar image"
+        />
+        <div className="card-body">
+          <h5 className="card-title text-body">{user.login}</h5>
+          <p className="card-text text-body">Score: {user.score}</p>
+          <a href={user.html_url} target="_blank" className="btn btn-dark">
+            Go to GitHub Profile
+          </a>
         </div>
       </div>
     </>
