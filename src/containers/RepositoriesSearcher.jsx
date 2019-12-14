01e0d6337cb9cd8198224ad1c62a8256118cdf81
MarioSalinas <mario_1605@live.com.mx>
2019-12-13 22:51:15 -0600

    feature/basic axios request to api

--- src/containers/RepositoriesSearcher.jsx
+++ src/containers/RepositoriesSearcher.jsx
@@ -1,8 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import SearchBar from "../components/SearcherBar.jsx";
 import RepositoryCard from "../components/RepositoryCard.jsx";
 
 const RepositoriesSearcher = () => {
+  const [repositoryHistory, setRepositoryHistory] = useState([]);
+  const page = 1;
+
+  async function fetchData(query) {
+    try {
+      const { data, status } = await axios({
+        url: `https://api.github.com/search/repositories?q=${query}&page=${page}&per_page=6`,
+        method: "get"
+      });
+      if (status === 200) {
+        console.log(data.items);
+        setuserHistory(data.items);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  fetchData("react");
+
   return (
     <div className="container-fluid m-0 p-5 bg-custom min-vh-100">
       <h4 className="text-center p-5 text-white title-responsive">
