cee304ecb4adc355eaaf2740ebb86daf83bac2bd
MarioSalinas <mario_1605@live.com.mx>
2019-12-14 12:59:56 -0600

    feature/basic-config-redux

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./routes/App.jsx";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import reducer from "./reducers";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./assets/styles/Text-queries.scss";
 
-ReactDOM.render(<App />, document.getElementById("app"));
+const initialState = {
+  user: "MarioSalinas1605"
+};
+
+const store = createStore(reducer, initialState);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("app")
+);
